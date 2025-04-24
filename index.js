ea0338cc0686657cd2742702d8d468bf65ff955f
Marion Saru <maghangasaru@gmail.com>
2025-04-24 14:28:58 +0300

    Completed optional lab

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let distance;
+let city;
+let tip;
+
+function scuberGreetingForFeet(distance){
+    if(distance <= 400){
+      return `This one is on me!`
+    }
+    else if(distance > 400 && distance <= 2000){
+      return `That will be twenty bucks.`
+    }
+    else if (distance > 2000 && distance <= 2500){
+    return `I will gladly take your thirty bucks.`
+   }
+   else {
+    return `No can do.`
+   }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case `generous`:
+      return `Thank you so much.`;
+    case `not as generous`:
+      return `Thank you.`;
+    default:
+      return `Bye.`;    
+  }
 }
+
+distance = 1;
+console.log(scuberGreetingForFeet(distance));
+
+city = "NYC"
+console.log(ternaryCheckCity(city));
+
+tip = "generous"
+console.log(switchOnCharmFromTip(tip));
